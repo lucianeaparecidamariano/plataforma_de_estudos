187978fe300ddad85041aed6a6a457364c6c75fa
lucianeaparecidamariano <lucianeapmarianogithub@gmail.com>
2022-10-25 01:14:48 -0300

    proffy

---
+++ README.md
@@ -0,0 +1,35 @@
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+ <img src="https://img.shields.io/static/v1?label=PRs&message=welcome&color=8257E5&labelColor=000000" alt="PRs welcome!" />
+
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=8257E5&labelColor=000000">
+</p>
+
+<br>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+
+## 💻 Projeto
+
+Proffy é uma plataforma de estudos online. Tem como objetivo conectar alunos e professores para estudos complementares, desenvolvimento de projetos e pesquisas.
+
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+---
+
+
+# plataformaonline
---
+++ src/database/fake_data.js
@@ -0,0 +1,37 @@
+// Dados
+const proffys = [
+    { 
+        name: "Luciane", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "14996872804", 
+        bio: "Entusiasta em Tecnologias Emergentes em Educação.<br><br>Apaixonado por tecnlogia e por mudar a vida das pessoas por meio da educação.",
+        subject: "HTML", 
+        cost: "20", 
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    { 
+        name: "Luciane", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "14996872804", 
+        bio: "Entusiasta em Tecnologias Emergentes em Educação.<br><br>Apaixonado por tecnlogia e por mudar a vida das pessoas por meio da educação.",
+        subject: "CSS", 
+        cost: "20", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    { 
+        name: "Luciane", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "14996872804", 
+        bio: "Entusiasta em Tecnologias Emergentes em Educação.<br><br>Apaixonado por tecnlogia e por mudar a vida das pessoas por meio da educação.",
+        subject: "JavaScript", 
+        cost: "20", 
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220]
+    }
+    
+]
